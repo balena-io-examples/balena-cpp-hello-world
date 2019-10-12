f4bb9dce79521f1a71d2fd08f6706b9b5d2f63d1
sidalpha1 <30462485+sidalpha1@users.noreply.github.com>
2019-10-12 13:29:26 +0530

    sid_comment_added

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,7 @@
 int main (int argc, char **argv) 
 {
 	printf("Hello, world!\n");
+//the printf function is used to print on the console.
+//In c++,you can use cout instead of printf.
 	return 0;
 }
